378f2d7136b7c4cb02f104c9c9119b62db1c9ce7
Sonikesh <91758635+SonikeshKumar14@users.noreply.github.com>
2023-03-08 23:39:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Navbar from './Navbar'
+import './App.css';
+
+function App() {
+  return (
+   <div>
+     <Navbar />
+     <div className="container">
+       <article>
+        <h1>What is Lorem Ipsum?</h1>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry...
+       </article>
+     </div>
+   </div>
+  );
+}
+
+export default App;
